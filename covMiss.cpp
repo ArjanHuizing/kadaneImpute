a8c45bcf8ba10a2ffa645d70245753ae39820598
Arjan Huizing <ArjanHJHuizing@gmail.com>
2019-08-04 01:16:26 +0200

    Added covariance function that works with missing data, using an E-M algorithm

---
+++ covMiss.cpp
@@ -0,0 +1,79 @@
+#include <RcppArmadillo.h>
+
+// [[Rcpp::depends("RcppArmadillo")]]
+using namespace arma;
+
+// Estimate the covariance matrix for a dataset with missing values using
+// maximum likelihood estimation. Values are obtained through a
+// Expectation-Maximization algorithm.
+
+// [[Rcpp::export]]
+mat covMiss(mat x, int its = 100) {
+  int n = x.n_rows;
+  double nrow = x.n_rows;
+  int m = x.n_cols;
+  int i, j, k;
+  vec means(m);
+  vec col(n);
+  vec row(m);
+  vec row_imp(m);
+  uvec avail(n);
+  uvec miss(n);
+  mat x_imp = x;
+  
+  // impute missings with means as starting values.
+  for(j = 0; j < m; j++){
+    col = x.col(j);
+    miss = find_nonfinite(col);
+    avail = find_finite(col);
+    // obtain means and impute
+    means[j] = mean(col.elem(avail));
+    col.elem(miss).fill(means[j]);
+    // assign imputed column
+    x_imp.col(j) = col;
+    means[j] = mean(col);
+  }
+  // starting values
+  mat sigma = cov(x_imp) * (nrow-1)/nrow;
+  mat bias(m, m);  
+  
+  // EM algorithm
+  for(j = 0; j < its; j++){
+    bias = zeros<mat>(m, m);
+    for(i = 0; i < n; i++){
+      row = conv_to< vec >::from(x.row(i));
+      row_imp = conv_to< vec >::from(x.row(i));
+      miss = find_nonfinite(row);
+      avail = find_finite(row);
+      if(m > avail.n_elem){
+        bias(miss, miss) = bias(miss, miss) + sigma(miss, miss); - 
+          sigma(miss, avail) * inv_sympd(sigma(avail, avail)) * sigma(avail, miss);
+        
+        row_imp(miss) = means(miss) + (sigma(miss, avail) * 
+          inv_sympd(sigma(avail, avail))) * (row(avail) - means(avail));
+        x_imp.row(i) = row_imp.t();  
+      }
+    }
+    
+    // update
+    for(k = 0; k < m; k++){
+      col = x_imp.col(k);
+      means[k] = mean(col);
+    }
+    sigma = (cov(x_imp) * (nrow-1)/nrow) + + (bias/nrow);
+  }
+  // return
+  return sigma;
+}
+
+// test
+/*** R
+set.seed(31598)
+testdat <- MASS::mvrnorm(100, mu = c(1, 0, 5), Sigma = matrix(c(1, 0, 0, 
+                                                             0, 1, 0,
+                                                             0, 0, 1), ncol = 3))
+testdat[1:50, 1] <- NA
+testdat[51:100, 2] <- NA
+# let it RIP
+covMiss(testdat)
+*/
